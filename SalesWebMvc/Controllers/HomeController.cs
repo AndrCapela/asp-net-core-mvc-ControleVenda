ada30249b80cdbb6a96917df6c6b939d59491804
André Capela <andr.capela@gmail.com>
2018-09-16 11:52:30 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
+
 
 namespace SalesWebMvc.Controllers {
     public class HomeController : Controller {
@@ -14,7 +15,6 @@ namespace SalesWebMvc.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
